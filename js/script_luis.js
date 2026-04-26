da59e778883c6035d99f15f0555eb8b46ed6f902
Specterman Luis <lspecte@gmail.com>
2026-04-26 20:38:10 -0300

    agregado seccion musica y datos personales

---
+++ js/script_luis.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  const section = document.querySelector(".sobre-mi__content");
+
+  setTimeout(() => {
+    section.classList.add("visible");
+  }, 200);
+});
